21b7e1dd59063d2d0ea9a90077097511f77d5e76
GuilhermeA <guilhermeciello@gmail.com>
2023-03-31 19:40:16 -0400

    Script para verificar se o apache esta rodando na instacia e enviar as informacoes para um diretorio EFS

--- scriptVerificaApache.sh
+++ scriptVerificaApache.sh
@@ -1,18 +1,23 @@
 #! bin/bash 
 
-data=$(date + "%d,%m,%y")
-
-dataHora=$(date + "%H,%M,%S")
-
-status=$(systemctl status httpd)
-
 nomeServico="Apache"
+data=$(date + "%d/%m/%y")
+dataHora=$(date + "%H:%M:%S")
+status=$(systemctl status httpd)
+touch statusApache2.txt
  
+#condicao para verifificar se o apache esta rodando na instancia ec2 
+
 if systemctl status httpd | grep -q "active (running)";
 
 then
 
-echo "O servico $nomeServico esta rodando na data $data $dataHora com o status $status"
+#envia as informacoes para o diretorio EFS 
+
+echo "O servico $nomeServico esta rodando na data $data $dataHora com o status $status" >> /mnt/efs/guilherme/online/statusApache.txt
 
 else
-echo "O servico $nomeServico esta com o status offline na data $data e $data Hora"
+echo "O servico $nomeServico esta com o status offline na data $data e $data Hora" >> statusApache2.txt
+fi
+#envia as informacoes para outro diretorio EFS caso o apache esta offline
+mv statusApache2.txt /mnt/efs/guilherme/offline
